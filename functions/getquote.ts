665770447b01d1959e5a45597eda9e44fe24f2ad
afif ahmed <afifahmed456123@gmail.com>
2021-06-11 13:51:57 +0530

    :sparkles: new function to get a quote

---
+++ functions/getquote.ts
@@ -0,0 +1,48 @@
+import {
+	Handler,
+	HandlerEvent,
+	HandlerContext,
+	HandlerResponse,
+} from "@netlify/functions";
+import { MongoClient } from "mongodb";
+
+const getData = async () => {
+	let client;
+	try {
+		client = new MongoClient("mongodb://localhost:27017/rickandmorty", {
+			useUnifiedTopology: true,
+		});
+		await client.connect();
+		const data = await client
+			.db("rickandmorty")
+			.collection("quotes")
+			.findOne({});
+		return { data: data };
+	} catch (error) {
+		console.log(error.message);
+		return {
+			error: error.message,
+		};
+	} finally {
+		await client.close();
+	}
+};
+
+const handler: Handler = async (
+	event: HandlerEvent,
+	context: HandlerContext
+): Promise<HandlerResponse> => {
+	const { data, error } = await getData();
+	if (error) {
+		return {
+			statusCode: 400,
+			body: JSON.stringify(error),
+		};
+	}
+	return {
+		statusCode: 200,
+		body: JSON.stringify(data),
+	};
+};
+
+export { handler };
